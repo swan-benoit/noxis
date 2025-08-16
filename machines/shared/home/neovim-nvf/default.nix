725a65886a50967a093e78ba6d6a6e6fcb8550e4
Swan BENOIT <swan.benoit@gmail.com>
2025-08-17 00:18:51 +0200

    config vim

--- machines/shared/home/neovim-nvf/default.nix
+++ machines/shared/home/neovim-nvf/default.nix
@@ -1,10 +1,154 @@
-{ pkgs, ...}:
+{pkgs, ...}: {
+  programs.nvf = {
+    enable = true;
+    enableManpages = true;
 
-{
+    settings.vim = {
+      viAlias = true;
+      vimAlias = true;
+      debugMode = {
+        enable = false;
+        level = 16;
+        logFile = "/tmp/nvim.log";
+      };
 
-	programs.nvf = {
-		enable = true;
-                enableManpages = true;
-	};
+      notes.neorg = {
+        enable = true;
+        setupOpts.load."core.defaults".enable = true;
+      };
 
+      lsp = {
+        # This must be enabled for the language modules to hook into
+        # the LSP API.
+        enable = true;
+
+        formatOnSave = true;
+        lspkind.enable = false;
+        lightbulb.enable = true;
+        lspsaga.enable = false;
+        trouble.enable = true;
+        lspSignature.enable = false;
+        otter-nvim.enable = true;
+        nvim-docs-view.enable = true;
+      };
+
+      languages = {
+        enableFormat = true;
+        enableTreesitter = true;
+        enableExtraDiagnostics = true;
+
+        nix = {
+          enable = true;
+        };
+
+        lua = {
+          enable = true;
+        };
+
+        markdown.enable = true;
+
+        java.enable = true;
+      };
+
+      debugger = {
+        nvim-dap = {
+          enable = true;
+          ui.enable = true;
+        };
+      };
+
+      visuals = {
+        nvim-scrollbar.enable = true;
+        nvim-web-devicons.enable = true;
+        nvim-cursorline.enable = true;
+        cinnamon-nvim.enable = true;
+        fidget-nvim.enable = true;
+
+        highlight-undo.enable = true;
+        indent-blankline.enable = true;
+
+        cellular-automaton.enable = false;
+      };
+
+      statusline = {
+        lualine = {
+          enable = true;
+          theme = "catppuccin";
+        };
+      };
+
+      theme = {
+        enable = true;
+        name = "catppuccin";
+        style = "mocha";
+        transparent = false;
+      };
+
+      autopairs.nvim-autopairs.enable = true;
+
+      autocomplete = {
+        blink-cmp.enable = true;
+      };
+      snippets.luasnip.enable = true;
+
+      filetree = {
+        neo-tree = {
+          enable = true;
+        };
+      };
+
+      tabline = {
+        nvimBufferline.enable = true;
+      };
+
+      binds = {
+        whichKey.enable = true;
+        cheatsheet.enable = true;
+      };
+
+      telescope.enable = true;
+
+      git = {
+        enable = true;
+        gitsigns.enable = true;
+        gitsigns.codeActions.enable = false; # throws an annoying debug message
+        neogit.enable = true;
+      };
+      terminal = {
+        toggleterm = {
+          enable = true;
+          lazygit.enable = true;
+        };
+      };
+      dashboard = {
+        dashboard-nvim.enable = false;
+        alpha.enable = true;
+      };
+      ui = {
+        borders.enable = true;
+        noice.enable = true;
+        colorizer.enable = true;
+        modes-nvim.enable = false; # the theme looks terrible with catppuccin
+        illuminate.enable = true;
+        breadcrumbs = {
+          enable = true;
+          navbuddy.enable = true;
+        };
+        smartcolumn = {
+          enable = true;
+          setupOpts.custom_colorcolumn = {
+            # this is a freeform module, it's `buftype = int;` for configuring column position
+            nix = "110";
+            ruby = "120";
+            java = "130";
+            go = ["90" "130"];
+          };
+        };
+      };
+      fastaction.enable = true;
+      comments = {
+        comment-nvim.enable = true;
+      };
+    };
+  };
 }
